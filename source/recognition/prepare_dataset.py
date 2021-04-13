e639bd587fc5fdfe97a0a910284c2ae4fe7ae00f
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2021-04-13 20:17:13 +0530

    prepare dataset for training
    - created lmdb function
    - define paths
    - separate IAM images & labels
    - include custom dataset
    - remove broken images
    - split data for training & validation
    - data augmentation
    - create batches

---
+++ source/recognition/prepare_dataset.py
@@ -0,0 +1,208 @@
+import os
+import cv2
+import lmdb
+import pickle
+import random
+import numpy as np
+from path import Path
+from utils import preprocess
+
+CURR_WORKDIR = os.getcwd()
+DATASET_DIR  = CURR_WORKDIR + '/dataset'
+IAM_DIR      = DATASET_DIR + '/IAM/img'
+LMDB_PATH    = Path(DATASET_DIR + '/IAM/lmdb')
+CUSTOM_DIR   = DATASET_DIR + '/CUSTOM/'
+CUSTOM_IMGS  = CUSTOM_DIR + 'ht_01'
+
+def createLMDB():
+    """For fast access when training on GPU"""
+
+    if not (LMDB_PATH).exists():
+        # 2GB is enough for IAM dataset
+        env = lmdb.open(str(LMDB_PATH), map_size=1024 * 1024 * 1024 * 2)
+
+        # go over all png files
+        fn_imgs = list((Path(IAM_DIR)).walkfiles('*.png'))
+        fn_imgs2 = list((Path(CUSTOM_IMGS)).walkfiles('*.png'))
+
+        # and put the imgs into lmdb as pickled grayscale imgs
+        with env.begin(write=True) as txn:
+
+            for i, fn_img in enumerate(fn_imgs):
+                print(i, len(fn_imgs))
+                img = cv2.imread(fn_img, cv2.IMREAD_GRAYSCALE)
+                basename = fn_img.basename()
+                txn.put(basename.encode("ascii"), pickle.dumps(img))
+
+            for j, fn_img2 in enumerate(fn_imgs2):
+                print(j, len(fn_imgs2))
+                img = cv2.imread(fn_img2, cv2.IMREAD_GRAYSCALE)
+                basename = fn_img2.basename()
+                txn.put(basename.encode("ascii"), pickle.dumps(img))
+
+        env.close()
+    else:
+        print("Skip LMDB creation")
+
+
+class Sample:
+    "sample from the dataset"
+
+    def __init__(self, gtText, filePath):
+        self.gtText = gtText
+        self.filePath = filePath
+
+class Batch:
+    "batch containing images and ground truth texts"
+
+    def __init__(self, gtTexts, imgs):
+        self.imgs = np.stack(imgs, axis=0)
+        self.gtTexts = gtTexts
+
+class DataLoaderIAM:
+    "loads data which corresponds to IAM format, see: http://www.fki.inf.unibe.ch/databases/iam-handwriting-database"
+
+    def __init__(self, data_dir, batchSize, imgSize, maxTextLen, fast=True):
+        "loader for dataset at given location, preprocess images and text according to parameters"
+
+        assert data_dir.exists()
+
+        self.fast = fast
+        if fast:
+            createLMDB()
+            self.env = lmdb.open(str(LMDB_PATH), readonly=True)
+
+        self.dataAugmentation = False
+        self.currIdx = 0
+        self.batchSize = batchSize
+        self.imgSize = imgSize
+        self.samples = []
+
+        f = open(data_dir / 'gt/words.txt')
+        chars = set()
+        # known broken images in IAM dataset
+        bad_samples_reference = ['a01-117-05-02', 'r06-022-03-05']
+
+        for line in f:
+            # ignore comment line
+            if not line or line[0] == '#':
+                continue
+
+            lineSplit = line.strip().split(' ')
+            assert len(lineSplit) >= 9
+
+            # filename: part1-part2-part3 --> part1/part1-part2/part1-part2-part3.png
+            fileNameSplit = lineSplit[0].split('-')
+            fileName = data_dir / 'img' / fileNameSplit[0] / f'{fileNameSplit[0]}-{fileNameSplit[1]}' / lineSplit[0] + '.png'
+
+            if lineSplit[0] in bad_samples_reference:
+                print('Ignoring known broken image:', fileName)
+                continue
+
+            # GT text are columns starting at 9
+            gtText = self.truncateLabel(' '.join(lineSplit[8:]), maxTextLen)
+            chars = chars.union(set(list(gtText)))
+
+            # put sample into list
+            self.samples.append(Sample(gtText, fileName))
+
+        # open custom dataset label
+        f2 = open(CUSTOM_DIR + 'ht_01_label.txt')
+
+        # append custom dataset to samples
+        for i, label in enumerate(f2):
+            label = label.strip('\n')
+            chars = chars.union(set(list(label)))
+            customPath = CUSTOM_DIR + f'ht_01/ht_{i}.png'
+
+            self.samples.append(Sample(label, customPath))
+
+        # split into training and validation set: 95% - 5%
+        splitIdx = int(0.95 * len(self.samples))
+        self.trainSamples = self.samples[:splitIdx]
+        self.validationSamples = self.samples[splitIdx:]
+
+        # put words into lists
+        self.trainWords = [x.gtText for x in self.trainSamples]
+        self.validationWords = [x.gtText for x in self.validationSamples]
+
+        # start with train set
+        self.trainSet()
+
+        # list of all chars in dataset
+        self.charList = sorted(list(chars))
+
+    def truncateLabel(self, text, maxTextLen):
+        # ctc_loss can't compute loss if it cannot find a mapping between text label and input
+        # labels. Repeat letters cost double because of the blank symbol needing to be inserted.
+        # If a too-long label is provided, ctc_loss returns an infinite gradient
+        cost = 0
+        for i in range(len(text)):
+            if i != 0 and text[i] == text[i - 1]:
+                cost += 2
+            else:
+                cost += 1
+            if cost > maxTextLen:
+                return text[:i]
+        return text
+
+    def trainSet(self):
+        "switch to randomly chosen subset of training set"
+
+        self.dataAugmentation = True
+        self.currIdx = 0
+        random.shuffle(self.trainSamples)
+        self.samples = self.trainSamples
+        self.currSet = 'train'
+
+    def validationSet(self):
+        "switch to validation set"
+
+        self.dataAugmentation = False
+        self.currIdx = 0
+        self.samples = self.validationSamples
+        self.currSet = 'val'
+
+    def getIteratorInfo(self):
+        "current batch index and overall number of batches"
+
+        if self.currSet == 'train':
+            # train set: only full-sized batches
+            numBatches = int(np.floor(len(self.samples) / self.batchSize))
+        else:
+            # val set: allow last batch to be smaller
+            numBatches = int(np.ceil(len(self.samples) / self.batchSize))
+        currBatch = self.currIdx // self.batchSize + 1
+        return currBatch, numBatches
+
+    def hasNext(self):
+        "iterator"
+
+        if self.currSet == 'train':
+            # train set: only full-sized batches
+            return self.currIdx + self.batchSize <= len(self.samples)
+        else:
+            # val set: allow last batch to be smaller
+            return self.currIdx < len(self.samples)
+
+    def getNext(self):
+        "iterator"
+
+        batchRange = range(self.currIdx, min(self.currIdx + self.batchSize, len(self.samples)))
+        gtTexts = [self.samples[i].gtText for i in batchRange]
+
+        imgs = []
+        for i in batchRange:
+            if self.fast:
+                with self.env.begin() as txn:
+                    basename = Path(self.samples[i].filePath).basename()
+                    data = txn.get(basename.encode("ascii"))
+                    img = pickle.loads(data)
+            else:
+                img = cv2.imread(self.samples[i].filePath, cv2.IMREAD_GRAYSCALE)
+
+            imgs.append(preprocess(img, self.imgSize, self.dataAugmentation))
+
+        self.currIdx += self.batchSize
+
+        return Batch(gtTexts, imgs)
---
+++ source/recognition/utils.py
@@ -0,0 +1,70 @@
+import cv2
+import random
+import numpy as np
+
+def preprocess(img, imgSize, dataAugmentation=False):
+    "put img into target img of size imgSize, transpose for TF and normalize gray-values"
+
+    # there are damaged files in IAM dataset - just use black image instead
+    if img is None:
+        img = np.zeros(imgSize[::-1])
+
+    # data augmentation
+    img = img.astype(np.float)
+
+    if dataAugmentation:
+        # photometric data augmentation
+        if random.random() < 0.25:
+            rand_odd = lambda: random.randint(1, 3) * 2 + 1
+            img = cv2.GaussianBlur(img, (rand_odd(), rand_odd()), 0)
+        if random.random() < 0.25:
+            img = cv2.dilate(img, np.ones((3, 3)))
+        if random.random() < 0.25:
+            img = cv2.erode(img, np.ones((3, 3)))
+        if random.random() < 0.5:
+            img = img * (0.25 + random.random() * 0.75)
+        if random.random() < 0.25:
+            img = np.clip(img + (np.random.random(img.shape) - 0.5) * random.randint(1, 50), 0, 255)
+        if random.random() < 0.1:
+            img = 255 - img
+
+        # geometric data augmentation
+        wt, ht = imgSize
+        h, w = img.shape
+        f = min(wt / w, ht / h)
+        fx = f * np.random.uniform(0.75, 1.25)
+        fy = f * np.random.uniform(0.75, 1.25)
+
+        # random position around center
+        txc = (wt - w * fx) / 2
+        tyc = (ht - h * fy) / 2
+        freedom_x = max((wt - fx * w) / 2, 0) + wt / 10
+        freedom_y = max((ht - fy * h) / 2, 0) + ht / 10
+        tx = txc + np.random.uniform(-freedom_x, freedom_x)
+        ty = tyc + np.random.uniform(-freedom_y, freedom_y)
+
+        # map image into target image
+        M = np.float32([[fx, 0, tx], [0, fy, ty]])
+        target = np.ones(imgSize[::-1]) * 255 / 2
+        img = cv2.warpAffine(img, M, dsize=imgSize, dst=target, borderMode=cv2.BORDER_TRANSPARENT)
+
+    # no data augmentation
+    else:
+        # center image
+        wt, ht = imgSize
+        h, w = img.shape
+        f = min(wt / w, ht / h)
+        tx = (wt - w * f) / 2
+        ty = (ht - h * f) / 2
+
+        # map image into target image
+        M = np.float32([[f, 0, tx], [0, f, ty]])
+        target = np.ones(imgSize[::-1]) * 255 / 2
+        img = cv2.warpAffine(img, M, dsize=imgSize, dst=target, borderMode=cv2.BORDER_TRANSPARENT)
+
+    # transpose for TF
+    img = cv2.transpose(img)
+
+    # convert to range [-1, 1]
+    img = img / 255 - 0.5
+    return img
